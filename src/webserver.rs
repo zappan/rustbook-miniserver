939b38cd394ec99db68fd42b67dfbb177524e4ac
Tomislav Capan <tomislav.capan@gmail.com>
2024-09-13 08:45:50 +0200

    Making server selectively responding to requests paths

--- src/webserver.rs
+++ src/webserver.rs
@@ -16,23 +16,46 @@ pub fn start() {
 fn handle_connection(mut stream: TcpStream) {
   let buf_reader = BufReader::new(&mut stream);
 
-  // The browser signals the end of an HTTP request by sending two newline characters in a row,
-  // so to get one request from the stream, we take lines until we get a line that is the empty string.
-  let http_request: Vec<_> = buf_reader
-    .lines()
-    .map(|result| result.unwrap())
-    .take_while(|line| !line.is_empty())
-    .collect();
+  // // The browser signals the end of an HTTP request by sending two newline characters in a row,
+  // // so to get one request from the stream, we take lines until we get a line that is the empty string.
+  // let http_request: Vec<_> = buf_reader
+  //   .lines()
+  //   .map(|result| result.unwrap())
+  //   .take_while(|line| !line.is_empty())
+  //   .collect();
+  //
+  // println!("Request: {:#?}", http_request);
 
-  println!("Request: {:#?}", http_request);
+  let request_line: String = buf_reader.lines().next().unwrap().unwrap();
+  println!("Request: {:#?}", request_line);
 
-  let resp_status = "HTTP/1.1 200 OK";
-  let resp_content = fs::read_to_string("hello.html").unwrap();
-  let content_len = resp_content.len();
+  // let (resp_status_line, content_file) = if request_line == "GET / HTTP/1.1" {
+  //   ("HTTP/1.1 200 OK", read_content_file("hello.html"))
+  // } else {
+  //   ("HTTP/1.1 404 NOT FOUND", read_content_file("404.html"))
+  // };
 
-  let response = format!(
+  let (resp_status_line, content) = match request_line.as_str() {
+    "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", read_content_file("hello.html")),
+    _ => ("HTTP/1.1 404 NOT FOUND", read_content_file("404.html")),
+  };
+
+  let response = build_response(resp_status_line, content);
+  send_response(stream, response);
+}
+
+fn read_content_file(filename: &str) -> String {
+  fs::read_to_string(filename).unwrap()
+}
+
+fn build_response(status_line: &str, content: String) -> String {
+  let content_len = content.len();
+  format!(
     "{}\r\nContent-Length: {}\r\n\r\n{}",
-    resp_status, content_len, resp_content
-  );
-  stream.write(response.as_bytes()).unwrap();
+    status_line, content_len, content
+  )
+}
+
+fn send_response(mut stream: TcpStream, response: String) {
+  stream.write_all(response.as_bytes()).unwrap();
 }
